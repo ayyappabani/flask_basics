a10f289a970e80c57822b9cfd3bafe2c6dbaf97a
ayyappabani <52236829+ayyappabani@users.noreply.github.com>
2020-09-08 10:58:26 +0530

    Update replicarun.sh

--- replicarun.sh
+++ replicarun.sh
@@ -2,5 +2,5 @@
 #!/bin/sh
 # This is a comment!
 #echo Hello World        # This is a comment, too!
-cp https://github.com/ayyappabani/flask_basics/blob/master/createfile1.yml /tmp
+#cp https://github.com/ayyappabani/flask_basics/blob/master/createfile1.yml /tmp
 kubectl create -f /tmp/replicaset1.yml
