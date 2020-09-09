0403615f8cf545bb4de54bafd8f9ec082a943a86
ayyappabani <52236829+ayyappabani@users.noreply.github.com>
2020-09-09 16:32:46 +0530

    Update and rename replicarun.sh to replicarun.yml

--- replicarun.sh
+++
@@ -1,6 +0,0 @@
-
-#!/bin/sh
-# This is a comment!
-#echo Hello World        # This is a comment, too!
-#cp https://github.com/ayyappabani/flask_basics/blob/master/createfile1.yml /tmp
-kubectl create -f /tmp/replicaset1.yml
